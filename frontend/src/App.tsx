ab4a15ff5bee6ad984d8905b92be0c62ed49a70f
claudiuludwig <claudiu.ludwig@student.upt.ro>
2022-01-10 00:22:53 +0200

    verificam

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -10,6 +10,9 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
+        <p>
+          ce e aici
+        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
