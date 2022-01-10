cd6a5a01e2fec9b5c71d8024f256c044a4128c3a
claudiuludwig <claudiu.ludwig@student.upt.ro>
2022-01-10 09:06:50 +0200

    working with interfaces

--- frontend/src/Pages/SignUp.tsx
+++ frontend/src/Pages/SignUp.tsx
@@ -1,58 +1,81 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Form, Col, Row, Button, Modal } from 'react-bootstrap'
 
-function SignUp()
-{
-    return(
-<>
-<Modal.Dialog>
-  <Modal.Header closeButton>
-    <Modal.Title>Înscrie-te</Modal.Title>
-  </Modal.Header>
 
-  <Modal.Body>
-    <>
-    <Form style={{margin:""}}>
-    <Row className="mb-3">
-    <Form.Group as={Col} controlId="formGridEmail">
-      <Form.Label>Email</Form.Label>
-      <Form.Control type="email" placeholder="Introdu emailul" />
-    </Form.Group>
+function SignUp() {
+    const [show, setShow] = useState(false);
+  
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+  
+    return (
+      <>
+        <Button variant="primary" onClick={handleShow}>
+          Aici e sign up
+        </Button>
+  
+        <Modal show={show} onHide={handleClose}>
+          <Modal.Header closeButton>
+            <Modal.Title>Înscrie-te</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+          <>
+                <Form style={{margin:""}}>
+            <Row className="mb-3">
+                <Form.Group as={Col}>
+                <Form.Label>Nume</Form.Label>
+                <Form.Control type="text" placeholder="Introdu nume" />
+                </Form.Group>
 
-    <Form.Group as={Col} controlId="formGridPassword">
-      <Form.Label>Password</Form.Label>
-      <Form.Control type="password" placeholder="Parolă" />
-    </Form.Group>
-  </Row>
+                <Form.Group as={Col}>
+                <Form.Label>Prenume</Form.Label>
+                <Form.Control type="text" placeholder="Introdu prenume" />
+                </Form.Group>
+            </Row>
 
-  <Form.Group className="mb-3" controlId="formGridAddress1">
-    <Form.Label>Adresa</Form.Label>
-    <Form.Control placeholder="strada, numărul etc." />
-  </Form.Group>
+            <Row className="mb-3">
+                <Form.Group as={Col} controlId="formGridEmail">
+                <Form.Label>Email</Form.Label>
+                <Form.Control type="email" placeholder="Introdu emailul" />
+                </Form.Group>
 
-  <Row className="mb-3">
-    <Form.Group as={Col} controlId="formGridCity">
-      <Form.Label>CNP</Form.Label>
-      <Form.Control />
-    </Form.Group>
+                <Form.Group as={Col} controlId="formGridPassword">
+                <Form.Label>Password</Form.Label>
+                <Form.Control type="password" placeholder="Parolă" />
+                </Form.Group>
+            </Row>
 
-    <Form.Group as={Col} controlId="formGridZip">
-      <Form.Label>Număr de telefon</Form.Label>
-      <Form.Control />
-    </Form.Group>
-  </Row>
-  </Form>
-  </>
-  </Modal.Body>
+            <Form.Group className="mb-3" controlId="formGridAddress1">
+                <Form.Label>Adresa</Form.Label>
+                <Form.Control placeholder="strada, numărul etc." />
+            </Form.Group>
 
-  <Modal.Footer>
-    <Button variant="secondary">Închide</Button>
-    <Button variant="primary">Salvează</Button>
-  </Modal.Footer>
-</Modal.Dialog>
+            <Row className="mb-3">
+                <Form.Group as={Col}>
+                <Form.Label>CNP</Form.Label>
+                <Form.Control />
+                </Form.Group>
 
-</>
-);
-}
+                <Form.Group as={Col}>
+                <Form.Label>Număr de telefon</Form.Label>
+                <Form.Control />
+                </Form.Group>
+            </Row>
+            </Form>
+            </>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button variant="secondary" onClick={handleClose}>
+              Închide
+            </Button>
+            <Button variant="primary" onClick={handleClose}>
+              Salvează
+            </Button>
+          </Modal.Footer>
+        </Modal>
+      </>
+    );
+  }
+  
 
 export default SignUp
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Home from './CityDangersAlertHome';
-import Title from './Components/Title';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Login from './Pages/Login'
 import SignUp from './Pages/SignUp'
 import HomeClient from './Pages/HomeClient'
 import HomeAdmin from './Pages/HomeAdmin'
+import { Title } from './Components/Title';
 
 ReactDOM.render(
   <React.StrictMode>
