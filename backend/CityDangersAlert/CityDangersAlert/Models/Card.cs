e3f2bd7744f398f0b3c16b7b82e2593eb7afdb79
claudiuludwig <claudiu.ludwig@student.upt.ro>
2022-01-12 07:24:14 +0200

    final project

---
+++ backend/CityDangersAlert/CityDangersAlert/Models/Card.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CityDangersAlert.Models
+{
+    public class Card
+    {
+        public int cardId { get; set; }
+        public string cardImage { get; set; }
+        public string cardTitle { get; set; }
+        public string cardSubtitle { get; set; }
+        public int memberId { get; set; }
+    }
+}
